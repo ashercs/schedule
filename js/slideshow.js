ff18f45301746dedca10672d9cb330e0e8965046
ashercs <105057569+ashercs@users.noreply.github.com>
2022-09-09 09:39:51 -0400

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,52 +1,52 @@
 const jsonData = {
   "schedule": [{
     "monday": [{
-      "1": "Homeroom",
-      "2": "Algebra II",
-      "3": "Programming",
-      "4": "Post-secondary Planning",
+      "1": "Homeroom (613)",
+      "2": "Algebra II (607)",
+      "3": "Programming (527)",
+      "4": "Post-secondary Planning (605)",
       "5": "Lunch",
-      "6": "Global II",
-      "7": "Biology",
-      "8": "Programming",
-      "9": "Global II"
+      "6": "global II (613)",
+      "7": "Biology (Lab 2)",
+      "8": "Programming (527)",
+      "9": "global II (613)"
     }],
     "tuesday": [{
-      "1": "Homeroom",
-      "2": "Algebra II",
-      "3": "Global II",
-      "4": "World Literature",
+      "1": "Homeroom (613)",
+      "2": "Algebra II (607)",
+      "3": "global II (613)",
+      "4": "World Literature (621)",
       "5": "Lunch",
-      "6": "Art Independent Study",
-      "7": "Movement",
-      "8": "Biology"
+      "6": "Art Independent Study (618)",
+      "7": "Movement (630)",
+      "8": "Biology (Lab 2)"
     }],
     "wednesday": [{
-      "1": "Homeroom",
-      "2": "Algebra II",
-      "3": "Global II",
-      "4": "World Literature",
+      "1": "Homeroom (613)",
+      "2": "Algebra II (607)",
+      "3": "global II (613)",
+      "4": "World Literature (621)",
       "5": "Lunch",
-      "6": "Art Independent Study",
-      "7": "Spanish",
-      "8": "Biology"
+      "6": "Art Independent Study (618)",
+      "7": "Spanish (627)",
+      "8": "Biology (Lab 2)"
     }],
     "thursday": [{
-      "1": "Homeroom",
-      "2": "Spanish",
+      "1": "Homeroom (613)",
+      "2": "Spanish (627)",
       "3": "ELA",
-      "4": "Counseling",
-      "5": "Programming",
+      "4": "Counseling (602)",
+      "5": "Programming (527)",
       "6": "Lunch",
       "7": "Student Life",
-      "8": "Biology",
-      "9": "Algebra II",
-      "10": "Advisory"
+      "8": "Biology (Lab 2)",
+      "9": "Algebra II (607)",
+      "10": "Advisory (613)"
     }],
     "friday": [{
-      "1": "Homeroom",
-      "2": "World Literature",
-      "3": "Spanish",
+      "1": "Homeroom (613)",
+      "2": "World Literature (621)",
+      "3": "Spanish (627)",
       "4": "Idea Lab"
     }]
   }]
@@ -54,7 +54,7 @@ const jsonData = {
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const d = new Date();
 let day = days[d.getDay()];
-console.log(jsonData['schedule'][0][day.toLowerCase()][0]['1'])
+// console.log(jsonData['schedule'][0][day.toLowerCase()][0]['1'])
 $(document).ready(function () {
   if (day !== "Monday" && day !== "Thursday" && day !== "Friday") {
     var links = [
